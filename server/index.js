e18c852f1ba85ee3bcbc248be471ec60dd6257d9
Lohith Bollineni <bollineni.lohith9@gmail.com>
2023-03-23 18:54:27 -0400

    async

--- server/index.js
+++ server/index.js
@@ -1,19 +1,36 @@
 const express = require('express')
+const path = require('path');
+const jokes = require('./controllers/jokes');
 const products = require('./controllers/products')
 const app = express()
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
-
+// Middleware
 app
-    .get('/', (req, res) => {
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
+
+
+// Actions
+app
+    .get('/api/v1/', (req, res) => {
         res.send('Hello World! From Express')
     })
-    .use('/products', products)
+    .use('/api/v1/products', products)
+    .use('/api/v1/jokes', jokes)
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
 
 
+console.log('1: About to start server')
 
 app.listen(port, () => 
-  console.log(`Server running at http://${hostname}:${port}/`)
+  console.log(`2: Server running at http://${hostname}:${port}/`)
 );
+
+console.log('3: Asked server to start')
