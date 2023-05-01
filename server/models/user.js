5505768d2b569d2e80cd3e06e5d5ce53032b327a
Lohith Bollineni <bollineni.lohith9@gmail.com>
2023-05-01 13:10:07 -0400

    database start

--- server/models/user.js
+++ server/models/user.js
@@ -93,15 +93,3 @@
 
 // process reduce
 // process reduc
-
-// process reduce
-// process reduce
-// process reduce
-// process reduce
-
-// server sxtart
-
-// process reduce
-// process reduce
-// process reduce
-// process reduce
